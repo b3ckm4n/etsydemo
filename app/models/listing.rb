bb594d2114e533bcf10fbc0af243dc85e2919bf3
Michael Beckman <b3ckm4n@gmail.com>
2015-07-27 21:47:10 -0700

    Removed path from prod env

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,8 +5,7 @@ class Listing < ActiveRecord::Base
 	else	
 		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.png",
 							:storage => :dropbox,
-						    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-						    :path => ":style/:id_:filename"
+						    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
 		validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 	end
 
